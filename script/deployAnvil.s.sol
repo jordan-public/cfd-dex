f3898ce8799f1e161faa6262baf09dab31e81809
jordan <jordan@bitola.com>
2023-03-21 02:04:26 -0400

    Deploy scripts

--- script/deployAnvil.s.sol
+++ script/deployAnvil.s.sol
@@ -7,28 +7,18 @@ import "../src/CFDOrderBook.sol";
 import "../src/CFDOrderBookFactory.sol";
 
 contract Deploy is Script {
-    // Polygon Mumbai
-    address constant USDC = 0x04068DA6C83AFCFA0e13ba15A6696662335D5B75;
+    // Gnosis Mainnet
+    address constant USDC = 0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83;
     
     address constant ORACLE_EUR_USD =
-        0x73366Fe0AA0Ded304479862808e02506FE556a98;
+        0xab70BCB260073d036d1660201e9d5405F5829b7a;
     address constant ORACLE_CHF_USD =
-        0xc76f762CedF0F78a439727861628E0fdfE1e70c2;
+        0xFb00261Af80ADb1629D3869E377ae1EEC7bE659F;
     address constant ORACLE_JPY_USD =
-        0xD647a6fC9BC6402301583C91decC5989d8Bc382D;
-    address constant ORACLE_CNY_USD =
-        0x04bB437Aa63E098236FA47365f0268547f6EAB32;
-    address constant ORACLE_AUD_USD =
-        0x062Df9C4efd2030e243ffCc398b652e8b8F95C6f;
-    address constant ORACLE_CAD_USD =
-        0xACA44ABb8B04D07D883202F99FA5E3c53ed57Fb5;
-    address constant ORACLE_GBP_USD =
-        0x099a2540848573e94fb1Ca0Fa420b00acbBc845a;
-    address constant ORACLE_HKD_USD =
-        0x82d43B72573f902F960126a19581BcBbA5b014F5;
-    address constant ORACLE_INR_USD =
-        0xDA0F8Df6F5dB15b346f4B8D1156722027E194E60;
-
+        0x2AfB993C670C01e9dA1550c58e8039C1D8b8A317;
+    address constant ORACLE_MXN_USD =
+        0xe9cea51a7b1b9B32E057ff62762a2066dA933cD2;
+    
     uint256 ENTRY_MARGIN =          100000000000000000; // 0.1 = 10%
     uint256 MAINTENANCE_MARGIN =     50000000000000000; // 0.05 = 5%
     uint256 LIQUIDATION_PENALTY =    20000000000000000; // 0.02 = 2%
@@ -65,37 +55,7 @@ contract Deploy is Script {
         }
 
         {
-            factory.createOrderBook(ORACLE_CNY_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
-            ICFDOrderBook p = factory.getOB(factory.getNumOBs() - 1); // Assuning no one runs this script concurrently
-            console.log("CFD Order Book", p.getDescription(), "deployed at:", address(p));
-        }
-
-        {
-            factory.createOrderBook(ORACLE_AUD_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
-            ICFDOrderBook p = factory.getOB(factory.getNumOBs() - 1); // Assuning no one runs this script concurrently
-            console.log("CFD Order Book", p.getDescription(), "deployed at:", address(p));
-        }
-
-        {
-            factory.createOrderBook(ORACLE_CAD_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
-            ICFDOrderBook p = factory.getOB(factory.getNumOBs() - 1); // Assuning no one runs this script concurrently
-            console.log("CFD Order Book", p.getDescription(), "deployed at:", address(p));
-        }
-
-        {
-            factory.createOrderBook(ORACLE_GBP_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
-            ICFDOrderBook p = factory.getOB(factory.getNumOBs() - 1); // Assuning no one runs this script concurrently
-            console.log("CFD Order Book", p.getDescription(), "deployed at:", address(p));
-        }
-
-        {
-            factory.createOrderBook(ORACLE_HKD_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
-            ICFDOrderBook p = factory.getOB(factory.getNumOBs() - 1); // Assuning no one runs this script concurrently
-            console.log("CFD Order Book", p.getDescription(), "deployed at:", address(p));
-        }
-
-        {
-            factory.createOrderBook(ORACLE_INR_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
+            factory.createOrderBook(ORACLE_MXN_USD, address(USDC), ENTRY_MARGIN, MAINTENANCE_MARGIN, LIQUIDATION_PENALTY, DUST);
             ICFDOrderBook p = factory.getOB(factory.getNumOBs() - 1); // Assuning no one runs this script concurrently
             console.log("CFD Order Book", p.getDescription(), "deployed at:", address(p));
         }
